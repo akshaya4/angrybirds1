106a17acbacee3a370091565e35d6bd44297e6eb
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-10-12 16:12:55 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -4,7 +4,7 @@ class Box{
      "restitution":0.8,
      "friction":0.3,
      "density":1.0
-      };
+      }
       this.body=Bodies.rectangle(x.y,width,height,options);
       this.width=width;
       this.height=height;
